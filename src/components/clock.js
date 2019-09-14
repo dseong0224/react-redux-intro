768cc6222516cfafbf8ebd9a02c094f8f30998cb
dseong0224 <dseong0224@gmail.com>
2019-09-14 00:34:28 +0000

    made redux clock

---
+++ src/components/clock.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { tick } from '../actions';
+
+class Clock extends Component {
+  componentDidMount(){
+    setInterval(this.props.tick, 1000);
+  }
+
+  render(){
+    console.log('clock props: ', this.props);
+    return (
+      <h1>{this.props.time}</h1>
+    )
+  }    
+}
+function mapStateToProps(state) {
+  return {
+    time: state.clock.time
+  }
+}
+
+export default connect(mapStateToProps, { tick } )(Clock);
